71c605dea7afd7c9a4fc4fda5a036670bea21df6
nnemich <nemich99@yandex.ru>
2023-05-16 16:09:44 +0300

    1st commit: delete imports

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
